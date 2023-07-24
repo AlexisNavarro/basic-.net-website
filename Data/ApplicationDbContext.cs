32ca8219e424c8ee6cdfa1b0cf9a623a8b3060d8
Alexis Navarro <44381479+AlexisNavarro@users.noreply.github.com>
2023-07-23 23:37:29 -0600

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokeWebApp2.Models;
+
+namespace JokeWebApp2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokeWebApp2.Models.Joke>? Joke { get; set; }
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokeWebApp2.Models
+{
+    public class Joke
+    {
+        public int ID { get; set; }
+        public String JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
